ab3c9fb6a4614020326d69adc40a22580a00cd1c
DiplineRU <artgul52@gmail.com>
2025-02-03 16:59:45 +0300

    initial commit

---
+++ src/main/java/com/example/Database_Swagger_Postman/DatabaseSwaggerPostmanApplication.java
@@ -0,0 +1,13 @@
+package com.example.Database_Swagger_Postman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseSwaggerPostmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseSwaggerPostmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Database_Swagger_Postman/DatabaseSwaggerPostmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Database_Swagger_Postman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseSwaggerPostmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
